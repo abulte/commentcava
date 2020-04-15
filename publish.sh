e994a264e76728fc3cc5a5eaaced6a949bf88525
Alexandre Bulté <alexandre@bulte.net>
2020-04-15 14:10:22 +0200

    deploy script

--- publish.sh
+++ publish.sh
@@ -3,6 +3,7 @@ source ~/nvs/nvs.sh
 nvs-use<.nvmrc
 yarn build
 cd ..
+git add dist/ && git commit dist/ -m 'build for deploy'
 docker-compose build
 docker tag commentcava_web abulte/commentcava
 docker push abulte/commentcava
