38495e54ab41e5770c738377903af70a56774631
Alexandre Bulté <alexandre@bulte.net>
2020-04-30 18:20:46 +0200

    cleanup sdeploy cript

--- publish.sh
+++ publish.sh
@@ -1,10 +1,4 @@
-cd frontend
-source ~/nvs/nvs.sh
-nvs-use<.nvmrc
-yarn build
-cd ..
-git add dist/ && git commit dist/ -m 'build for deploy'
 docker-compose build
 docker tag commentcava_web abulte/commentcava
 docker push abulte/commentcava
-ssh root@$CCV_SERVER "cd commentcava && docker service update --image 'abulte/commentcava:latest' commentcava_web"
+ssh $CCV_SERVER "$CCV_DEPLOY_COMMAND"
